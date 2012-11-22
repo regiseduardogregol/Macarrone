f2380ccb5759e8c1d5499a25c2b8ffed708cc3ab
regiseduardogregol <regiseduardogregol@gmail.com>
2012-11-22 11:13:00 -0200

    tentando mandar essa bosta pro heroku ehauehueah

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjetoMacarrao::Application.config.session_store :cookie_store, key: '_Projeto_Macarrao_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjetoMacarrao::Application.config.session_store :active_record_store
