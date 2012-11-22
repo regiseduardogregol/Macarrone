e5bf7312c0103a2be3227b9a68e9bdaf9a27ee23
regiseduardogregol <regiseduardogregol@gmail.com>
2012-11-22 12:09:08 -0200

    removendo Projeto_Macarrao

--- Projeto_Macarrao/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ProjetoMacarrao::Application.config.secret_token = '9a354a927aea6772e2998ddb2a7658d5de5a9d9e7d9b6358a81434ca3cea2711b850d3281fff879a6b2bd9a599c822522f6a8896caf4ad6e6e71e050974ac52c'
